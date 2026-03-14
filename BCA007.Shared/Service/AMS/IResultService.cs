002179a6b8d185774e1f8a127dd66adbf7130d73
adithyanhp <adithyanhpm@gmail.com>
2026-03-14 18:56:22 +0530

    Initial commit

---
+++ BCA007.Shared/Service/AMS/IResultService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BCA007.Shared.DTOs.AMS;
+
+namespace BCA007.Shared.Service.AMS
+{
+    public interface IResultService
+    {
+        Task<List<ResultViewDto>> GetAllAsync();
+        Task UpdateResults(List<ResultViewDto> dto);
+
+        Task<List<ResultViewDto>> GetResultsByExamId(int? examId);
+
+    }
+}
